32b1f2d9d82bb5b7f6b2a2b16ea04fa4cddb3ead
ziapk <zia.pccr@yahoo.com>
2015-04-24 23:19:36 +0500

    update daily progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function (argument) {
+	var n_place = $('.qty .value');
+	$('.qty').on('click', '.add', function() {
+	    var n = parseInt(n_place.text(), 10);
+	    n_place.empty().append(n + 1);
+	});
+	$('.qty').on('click', '.sub', function() {
+	    var n = parseInt(n_place.text(), 10);
+	    if(n != 1){
+	    	n_place.empty().append(Math.max(0, n - 1));	    	
+	    }
+	});
+});
